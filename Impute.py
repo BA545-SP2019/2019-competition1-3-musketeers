6ea12750ddd6b518d65425d6ddd5e86c4eb81ce8
khickey25 <kevin.hickey@student.fairfield.edu>
2019-03-05 18:07:19 +0000

    tuesday work

--- Impute.py
+++ Impute.py
@@ -20,3 +20,19 @@ def impute_median(df):
     data = df.apply(lambda x: x.fillna(x.median()),axis=0)
    
     return data
+
+
+def impute_knn(df, k_num):
+    '''impute missing values using a KNN implementation.
+    Using the Impyute library, use a KNN model to impute into missing values. 
+    Input a pandas dataframe of the Competition 1 dataset, along with the desired number of neighbors to consider.
+    Outputs a non-null pandas dataframe
+    .'''
+    
+    import pandas as pd
+    import  impyute.imputation
+    data = df.values
+    data = impyute.imputation.cs.fast_knn(df, k=k_num)
+    data = pd.DataFrame(data)
+    
+    return data
